84380a473386109aaff8bf7a5609f920a0fd01b3
ananda91 <biswas@iastate.edu>
2016-01-29 14:57:45 -0600

    random codes

---
+++ cpp_learn/if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "enter an integer" ;
+    cin >> a;
+    if(a==9)
+    {
+        cout << "Hello world!" << endl;
+    }
+
+    return 0;
+}
